902bbabf8771a3288b648f53e8413a62dbba2fa2
david <david.jitca@vodafone.com>
2023-07-28 15:51:56 +0300

    refactor code lint

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -93,6 +93,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @extend_schema_view(
     list=extend_schema(
         parameters=[
@@ -119,9 +120,13 @@ class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
         )
         queryset = self.queryset
         if assigned_only:
-            queryset = queryset.filter(recipe__isnull=False)
+            queryset = queryset.filter(
+                recipe__isnull=False
+            )
 
-        return queryset.filter(user=self.request.user).order_by('-name').distinct()
+        return queryset.filter(
+            user=self.request.user
+            ).order_by('-name').distinct()
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
